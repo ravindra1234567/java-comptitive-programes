583bad33d6573be498bdbb754ea874c7b787dddd
Ravindra Kumar Kushwaha <47721333+ravindra1234567@users.noreply.github.com>
2020-08-28 18:22:40 +0530

    Unique pairs sum given target

---
+++ UniquePairsSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class uniquePairsSum{
+    public static int uniquePairs(int[] nums, int target){
+        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> seen = new HashSet<Integer>();
+        int count = 0;
+        for(int num : nums){
+            if(set.contains(target-num) && !seen.contains(num)){
+                count++;
+                seen.add(target-num);
+                seen.add(num);
+            }
+            else if(!set.contains(num)){
+                set.add(num);
+            }
+
+        }
+
+        return count;
+    }
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int n,target;
+        n= scan.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        target = scan.nextInt();
+        System.out.println(uniquePairs(arr,target));
+    }
+    
+}
